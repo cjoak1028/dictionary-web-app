d02fd344dc4b045101afc466eccf82185acf1bc0
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-06-13 14:15:46 -0400

    added layout and styling for theme toggler

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import appLogo from "../assets/images/logo.svg";
 import FontPicker from "./FontPicker";
 import Divider from "./Divider";
+import ThemeToggler from "./ThemeToggler";
 
 export default function Header() {
   return (
     <header className="h-8 my-6 flex flex-row items-center justify-between">
-      <img src={appLogo} alt="logo depicting a dictionary" />
+      <img className="h-full" src={appLogo} alt="logo depicting a dictionary" />
       <div className="flex flex-row">
         <FontPicker />
         <Divider />
+        <ThemeToggler />
       </div>
     </header>
   );
