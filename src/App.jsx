8543c1e2177140d994d3f216cd133c48c5cf4bbe
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-06-14 14:26:36 -0400

    created search bar and its active states

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header.jsx";
+import SearchForm from "./components/SearchForm.jsx";
 
 function App() {
   return (
     <div className="px-6">
       <Header />
-      <main></main>
+      <main>
+        <SearchForm />
+        Search Output
+      </main>
     </div>
   );
 }
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import searchIcon from "../assets/images/icon-search.svg";
+
+export default function SearchForm() {
+  const [inputFocus, setInputFocus] = useState(false);
+
+  function handleInputFocus() {
+    setInputFocus(true);
+  }
+
+  function handleInputBlur() {
+    setInputFocus(false);
+  }
+
+  return (
+    <form
+      className={`h-12 rounded-2xl bg-gray-100 px-6 flex flex-row items-center gap-3 ${
+        inputFocus ? "outline outline-1 outline-purple-500" : ""
+      }`}
+    >
+      <input
+        className="flex-1 w-full bg-gray-100 text-base font-bold focus:outline-0 caret-purple-500"
+        type="search"
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+      />
+      <span>
+        <img
+          className="w-4 h-4"
+          src={searchIcon}
+          alt="icon depicting a magnifying glass"
+        />
+      </span>
+    </form>
+  );
+}
