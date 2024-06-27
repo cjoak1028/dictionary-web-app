97e1d35d9949cefdcd1efe385e06e510256c8f89
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-06-26 21:45:23 -0400

    Created dropdown menu

--- src/components/FontPicker.jsx
+++ src/components/FontPicker.jsx
@@ -1,14 +1,50 @@
-import arrowDownIcon from "../assets/images/icon-arrow-down.svg";
+import { useState } from "react";
 
 export default function FontPicker() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleMenuClick() {
+    setShowMenu((prevShowMenu) => !prevShowMenu);
+  }
+
   return (
-    <>
-      <button className="flex flex-row items-center gap-4">
+    <div className="relative">
+      <button
+        className="flex flex-row items-center gap-4"
+        onClick={handleMenuClick}
+      >
         <span className="text-sm font-bold">Sans Serif</span>
-        <span>
-          <img src={arrowDownIcon} />
-        </span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="8"
+          viewBox="0 0 14 8"
+        >
+          <path
+            fill="none"
+            stroke="#A445ED"
+            strokeWidth="1.5"
+            d="m1 1 6 6 6-6"
+          />
+        </svg>
       </button>
-    </>
+      {showMenu && (
+        <div className="absolute bg-surface dark:bg-surface-container-dark right-0 top-9 p-6 w-36 rounded-2xl shadow-[0_5px_30px_0_rgba(0,0,0,0.1)] dark:shadow-[0_5px_30px_0_rgba(164,69,237,1)] text-sm font-bold">
+          <ul className="flex flex-col gap-4">
+            <li>
+              <button className="font-sans hover:text-primary">
+                Sans Serif
+              </button>
+            </li>
+            <li>
+              <button className="font-serif hover:text-primary">Serif</button>
+            </li>
+            <li>
+              <button className="font-mono hover:text-primary">Mono</button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
