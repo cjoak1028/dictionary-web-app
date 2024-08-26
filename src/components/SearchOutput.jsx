9761b16edc6695e52710e1c213e46167a2eee064
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-08-25 22:20:28 -0400

    Moved word fetch logic into custom hook called useFetchWord

--- src/components/SearchOutput.jsx
+++ src/components/SearchOutput.jsx
@@ -2,38 +2,11 @@ import { useEffect, useState } from "react";
 import Meaning from "./Meaning.jsx";
 import Phonetic from "./Phonetic.jsx";
 import Source from "./Source.jsx";
+import useFetchWord from "../hooks/useFetchWord.js";
 
 export default function SearchOutput({ searchWord }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchWordData(word) {
-      const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
-        }
-
-        const wordData = await response.json();
-        setData(wordData[0]);
-
-        // REMOVE TIMER
-        setTimeout(() => {
-          setLoading(false);
-        }, "1000");
-      } catch (error) {
-        console.error(error.message);
-        setLoading(false);
-      }
-    }
-
-    if (searchWord) {
-      setLoading(true);
-      fetchWordData(searchWord);
-    }
-  }, [searchWord]);
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
+  const [data, loading, error] = useFetchWord(url);
 
   // REMOVE
   useEffect(() => {
